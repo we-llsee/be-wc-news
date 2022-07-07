8020b058c1535863858c9d24b5b6192b92f37560
will <will@wills-MacBook-Pro.local>
2022-07-07 12:25:21 +0100

    complete /api/articles endpoint

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -44,3 +44,13 @@ exports.getUsers=(req,res,next) => {
         return res.status(200).send({users})
     });
 }
+
+exports.getArticles=(req,res,next) => {
+    models.fetchArticles().then(articles=>{
+        articles=articles.map((article)=> {
+            article.comment_count = +article.comment_count;
+            return article;
+        })
+        res.status(200).send({articles});
+    });
+}
--- models/models.js
+++ models/models.js
@@ -1,5 +1,5 @@
 const db=require('../db/connection.js');
-const query=require('pg-format')
+const format=require('pg-format')
 
 exports.fetchTopics=() =>{
     return db.query('SELECT * FROM topics').then(({rows}) => {
@@ -22,4 +22,14 @@ exports.updateArticleById = (inc_votes,article_id) => {
         if(!result.rows[0]) return Promise.reject({status:404,msg:'Non-existent article_id'});
         return result;
     });
+}
+
+exports.fetchArticles=()=>{
+    const formattedQuery=format(`SELECT articles.*, COUNT(comments.comment_id) AS comment_count FROM articles
+    LEFT JOIN comments 
+    ON articles.article_id=comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC`);
+
+    return db.query(formattedQuery).then(({rows})=> rows);
 }
