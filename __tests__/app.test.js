2025b16d05e9d92e835ce179e58b5ba5fe004584
will <will@wills-MacBook-Pro.local>
2022-07-29 15:00:13 +0100

    add limit and page queries to endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -817,5 +817,101 @@ describe('Express app',() => {
             })
         })
     })
+
+    describe('GET /api/topics pagination',()=>{
+
+        it('200 /api/topics limit is 10 rows by default',()=>{
+            return db.query(`INSERT INTO topics
+            (slug,description)
+            VALUES
+            ('4','4'),
+            ('5','5'),
+            ('6','6'),
+            ('7','7'),
+            ('8','8'),
+            ('9','9'),
+            ('10','10'),
+            ('11','11'),
+            ('12','12')
+            `).then(()=>{
+               return request(app).get('/api/topics').expect(200)
+            }).then(({body})=>{
+                expect(body.topics.length).toBe(10);
+            });
+        })
+
+        it('200 /api/topics?limit=7 returns 7 rows',()=>{
+            return request(app).get('/api/topics?limit=7').expect(200).then(({body})=>{
+                expect(body.topics.length).toBe(7);
+            });
+        })
+
+        it('200 /api/topics?limit=3 hardcoded - default p is 1',()=>{
+            return db.query(`DELETE FROM comments`).then(()=>{
+                return db.query(`DELETE FROM articles`)
+            }).then(()=>{
+                return db.query(`DELETE FROM topics`)
+            }).then(()=>{
+                return db.query(`INSERT INTO topics
+                (slug,description)
+                VALUES
+                ('4','4'),
+                ('5','5'),
+                ('6','6'),
+                ('7','7'),
+                ('8','8'),
+                ('9','9'),
+                ('10','10'),
+                ('11','11'),
+                ('12','12')`)
+            }).then(()=>{
+                return request(app).get('/api/topics?limit=3').expect(200)
+            }).then(({body})=>{
+                expect(body).toEqual({topics:[
+                    {slug:'4',description:'4'},{slug:'5',description:'5'},{slug:'6',description:'6'}]
+                })
+            })
+            //keep hardcoded test below this one
+        })
+
+        it('200 /api/topics?limit=3&p=2 hardcoded - returns 4th,5th and 6th topics to be added to table',()=>{
+           //this test should proceed commented test
+            return request(app).get('/api/topics?limit=3&p=2').expect(200).then(({body})=>{
+                expect(body).toEqual({topics:[
+                    {slug:'7',description:'7'},{slug:'8',description:'8'},{slug:'9',description:'9'}]
+                })
+            })    
+        })
+
+        it('200 /api/topics?limit=7&p=2000 returns {topics:[]}',()=>{
+            return request(app).get('/api/topics?limit=7&p=2000').expect(200).then(({body})=>{
+                expect(body).toEqual({topics:[]})
+            });
+        })
+
+        it('400 /api/topics?limit=duck returns {msg: Invalid limit query}',()=>{
+            return request(app).get('/api/topics?limit=duck').expect(400).then(({body})=>{
+                expect(body).toEqual({msg:'Invalid limit query'})
+            })
+        })
+
+        it('400 /api/topics?limit=eight returns {msg: Invalid limit query}',()=>{
+            return request(app).get('/api/topics?limit=eight').expect(400).then(({body})=>{
+                expect(body).toEqual({msg:'Invalid limit query'})
+            })
+        })
+
+        it('400 /api/topics?p=tiger returns {mgs: Invalid page query}',()=>{
+            return request(app).get('/api/topics?p=tiger').expect(400).then(({body})=>{
+                expect(body).toEqual({msg:'Invalid page query'})
+            });
+        })
+
+        it('400 /api/topics?p=-4 returns {mgs: Invalid page query}',()=>{
+            return request(app).get('/api/topics?p=-4').expect(400).then(({body})=>{
+                expect(body).toEqual({msg:'Invalid page query'})
+            });
+        })
+    })
 });
 
